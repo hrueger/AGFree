c131fd8b69e10002580f7fee90b11a085c912623
H. Rüger <hannesrueger@gmx.de>
2020-09-20 19:48:27 +0200

    (web) fixed some linting errors

--- AGFree/createFilesWithApiKeys.js
+++ AGFree/createFilesWithApiKeys.js
@@ -1,21 +1,21 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const files = {
-    "src/environments/environment.ts": 'export const environment = {apiUrl: "", appUrl: "",production: false};',
-    "src/environments/environment.prod.ts": 'export const environment = {apiUrl: "", appUrl: "",production: true};',
+    "src/environments/environment.ts": "export const environment = {apiUrl: \"\", appUrl: \"\",production: false};",
+    "src/environments/environment.prod.ts": "export const environment = {apiUrl: \"\", appUrl: \"\",production: true};",
 };
-var args = process.argv.slice(2);
+const args = process.argv.slice(2);
 counter = 0;
 for (key in files) {
     if (!fs.existsSync(key)) {
-        var value = files[key];
+        let value = files[key];
         if (args[counter]) {
-            value = Buffer.from(args[counter], 'base64').toString('ascii');
+            value = Buffer.from(args[counter], "base64").toString("ascii");
         }
         fs.writeFileSync(key, value);
         console.info("File", key, "created, please insert your credentials there!");
     } else {
         console.info("File", key, "skipped, as it already exists!");
     }
-    counter = counter + 1;
+    counter += 1;
 }
