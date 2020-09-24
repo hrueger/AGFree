9e8fbbdc105dc39af4c262b0f4232c8355a841c2
H. Rüger <hannesrueger@gmx.de>
2020-09-24 12:14:08 +0200

    (web) (schedule) added `small` mode

--- AGFree/src/app/_components/schedule/schedule.component.ts
+++ AGFree/src/app/_components/schedule/schedule.component.ts
@@ -3,12 +3,14 @@ import { RemoteService } from "../../_services/remote.service";
 
 type Period = {
     name: string;
+    nameShort: string;
     id: number;
     dayId?: number;
 };
 
 type Day = {
     name: string;
+    nameShort: string;
     id: number;
 };
 
@@ -27,81 +29,100 @@ type Userdata = {
 export class ScheduleComponent {
     @Input() public edit: false;
     @Input() public userdata: Userdata;
+    @Input() public small = false;
     public saving = false;
     public days: Day[] = [
         {
             id: 1,
             name: "Montag",
+            nameShort: "Mo",
         },
         {
             id: 2,
             name: "Dienstag",
+            nameShort: "Di",
         },
         {
             id: 3,
             name: "Mittwoch",
+            nameShort: "Mi",
         },
         {
             id: 4,
             name: "Donnerstag",
+            nameShort: "Do",
         },
         {
             id: 5,
             name: "Freitag",
+            nameShort: "Fr",
         },
     ];
     public periods: Period[] = [
         {
             id: 1,
             name: "1. Stunde",
+            nameShort: "1.",
         },
         {
             id: 2,
             name: "2. Stunde",
+            nameShort: "2.",
         },
         {
             id: 3,
             name: "3. Stunde",
+            nameShort: "3.",
         },
         {
             id: 4,
             name: "Pause",
+            nameShort: "--",
         },
         {
             id: 5,
             name: "4. Stunde",
+            nameShort: "4.",
         },
         {
             id: 6,
             name: "5. Stunde",
+            nameShort: "5.",
         },
         {
             id: 7,
             name: "6. Stunde",
+            nameShort: "6.",
         },
         {
             id: 8,
             name: "Mittagspause",
+            nameShort: "--",
         },
         {
             id: 9,
             name: "7. Stunde",
+            nameShort: "7.",
         },
         {
             id: 10,
             name: "8. Stunde",
+            nameShort: "8.",
         },
         {
             id: 11,
             name: "Pause",
+            nameShort: "--",
         },
         {
             id: 12,
             name: "9. Stunde",
+            nameShort: "9.",
         },
         {
             id: 13,
             name: "10. Stunde",
+            nameShort: "10.",
         },
     ];
 
@@ -163,7 +184,7 @@ export class ScheduleComponent {
             return;
         }
         this.saving = true;
-        this.remoteService.post("/users/schedule", { data: this.userdata }).subscribe((d) => {
+        this.remoteService.post("/users/schedule", { data: this.userdata }).subscribe(() => {
             this.saving = false;
         }, () => {
             this.saving = false;
