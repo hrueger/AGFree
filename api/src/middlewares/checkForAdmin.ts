0df761eb0075b6e6f8f8426dd00bba9305576e7c
Hannes Rüger <hannesrueger@gmx.de>
2020-09-27 14:12:43 +0200

    (api) added checkForAdmin middleware

---
+++ api/src/middlewares/checkForAdmin.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express";
+import * as i18n from "i18n";
+import { getRepository } from "typeorm";
+
+import { User } from "../entity/User";
+
+export const checkForAdmin = async (req: Request, res: Response, next: NextFunction):
+    Promise<void> => {
+    const id = res.locals.jwtPayload.userId;
+
+    const userRepository = getRepository(User);
+    let user: User;
+    try {
+        user = await userRepository.findOneOrFail(id);
+    } catch {
+        res.status(401).send({ message: i18n.__("errors.userNotFound"), logout: true });
+    }
+
+    if (user && user.isAdmin) {
+        next();
+    } else {
+        res.status(401).send({ message: i18n.__("errors.notAllowed") });
+    }
+};
