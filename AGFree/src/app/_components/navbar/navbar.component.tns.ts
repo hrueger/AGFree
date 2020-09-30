f45e9622cc72864816eaff29c703f4162f18359e
Hannes Rüger <hannesrueger@gmx.de>
2020-09-30 19:23:25 +0200

    (mobile) (navbar) added modal mode

--- AGFree/src/app/_components/navbar/navbar.component.tns.ts
+++ AGFree/src/app/_components/navbar/navbar.component.tns.ts
@@ -11,12 +11,18 @@ import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 })
 export class NavbarComponent {
     @Input() public title = "AGView";
+    @Input() public modal = false;
     @Input() public navbarItems: {
         text: string;
         id: string;
     }[] = [];
     @Output() public itemTap: EventEmitter<string> = new EventEmitter<string>();
+    @Output() public modalBack: EventEmitter<void> = new EventEmitter<void>();
     onDrawerButtonTap(): void {
+        if (this.modal) {
+            this.modalBack.emit();
+            return;
+        }
         const sideDrawer = Application.getRootView() as RadSideDrawer;
         sideDrawer.showDrawer();
     }
