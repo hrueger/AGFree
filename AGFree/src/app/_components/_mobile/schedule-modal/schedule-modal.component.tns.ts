c5d4712034ac255fda8308e2499b27f7eeed259a
Hannes Rüger <hannesrueger@gmx.de>
2020-10-01 17:13:08 +0200

    (mobile) view users and their schedules

--- AGFree/src/app/_components/_mobile/schedule-modal/schedule-modal.component.tns.ts
+++ AGFree/src/app/_components/_mobile/schedule-modal/schedule-modal.component.tns.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { RouterExtensions } from "@nativescript/angular";
 import { User } from "../../../_models/User";
+import { StorageService } from "../../../_services/storage.service";
 
 @Component({
     selector: "app-schedule-modal",
@@ -11,12 +12,26 @@ import { User } from "../../../_models/User";
 export class ScheduleModalComponent {
     public title = "";
     public user: User;
-    constructor(private router: RouterExtensions, private route: ActivatedRoute) { }
+    public usersToShow: User[];
+    public selectedPeriod: number;
+    public selectedDay: number;
+    constructor(
+        private router: RouterExtensions,
+        private route: ActivatedRoute,
+        private storageService: StorageService,
+    ) {}
 
     public ngOnInit(): void {
         if (this.route.snapshot.queryParams.user) {
             this.user = JSON.parse(this.route.snapshot.queryParams.user);
             this.title = this.user.username;
+        }
+        if (this.route.snapshot.queryParams.selectedDay
+            && this.route.snapshot.queryParams.selectedPeriod) {
+            this.usersToShow = JSON.parse(this.route.snapshot.queryParams.usersToShow);
+            this.title = this.route.snapshot.queryParams.title;
+            this.selectedDay = this.route.snapshot.queryParams.selectedDay;
+            this.selectedPeriod = this.route.snapshot.queryParams.selectedPeriod;
         }
     }
 
